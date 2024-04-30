1d0e98e73d16f2f4ebf2d18e571bf561f3d8b10b
BearnMountain <161377261+BearnMountain@users.noreply.github.com>
2024-04-29 23:47:33 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,6 @@
 #include "spaghetticlattices.cpp"
 
 // examples of initializing values for noisemap
-
-SpagettiLattices* map;
-Perlin* map;
 const uint16_t MAP_HEIGHT = 1000;
 const uint16_t MAP_WIDTH  = 1000;
 cosnt uint8_t frequency = 20; // dictates detail, larger number means larger area to smoothly transition through.
@@ -12,8 +9,16 @@ uint8_t noisemap[MAP_HEIGHT][MAP_WIDTH]={{0}};
 unsigned int seed = 235329234; // 9 digits seeds for randomization
 
 
+
+// noise maps
+
+SpagettiLattices* map_spaghetti;
+Perlin* map_perlin;
+
 int main(){
-  map = new Perlin(MAP_WIDTH, MAP_HEIGHT, &noisemap[0][0], frequency, seed); // populates the noisemap with 2^8 integer values
+  map_perlin    =           new Perlin(MAP_WIDTH, MAP_HEIGHT, &noisemap[0][0], frequency, seed); // populates the noisemap with 2^8 integer values
+  map_spaghetti = new SpagettiLattices(MAP_WIDTH, MAP_HEIGHT, &noisemap[0][0], frequency, seed);
+
   
   return 0;
 }
