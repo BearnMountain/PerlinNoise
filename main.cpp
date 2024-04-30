b8622b6485fd51200ca965ceee6fa6ecd399f164
BearnMountain <161377261+BearnMountain@users.noreply.github.com>
2024-04-29 23:36:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 Perlin* map;
 const uint16_t MAP_HEIGHT = 1000;
 const uint16_t MAP_WIDTH  = 1000;
+cosnt uint8_t frequency = 20; // dictates detail, larger number means larger area to smoothly transition through.
 uint8_t noisemap[MAP_HEIGHT][MAP_WIDTH]={{0}};
 unsigned int seed = 235329234; // 9 digits seeds for randomization
 
+
 int main(){
-  map = new Perlin(MAP_WIDTH, MAP_HEIGHT, &noisemap[0][0], 20, seed); // populates the noisemap with 2^8 integer values
+  map = new Perlin(MAP_WIDTH, MAP_HEIGHT, &noisemap[0][0], frequency, seed); // populates the noisemap with 2^8 integer values
   
   return 0;
 }
